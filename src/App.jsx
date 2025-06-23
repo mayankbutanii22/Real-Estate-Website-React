1559cc5f87f34f7abdb60ae06814b94cd26a2b32
mayankbutanii22 <mayankbutanii22@gmail.com>
2025-06-23 09:09:20 +0530

    Real-Estate-Website-React

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+
+import Home from './Pages/Home';
+import Properties from './Pages/Properties';
+import PropertyDetail from './Pages/PropertyDetail';
+import Agents from './Pages/Agents';
+import Contact from './Pages/Contact';
+
+const App = () => {
+  return (
+     <>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/properties" element={<Properties />} />
+          <Route path="/property/:id" element={<PropertyDetail />} />
+          <Route path="/agents" element={<Agents />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+     </>
+  );
+};
+
+export default App;
